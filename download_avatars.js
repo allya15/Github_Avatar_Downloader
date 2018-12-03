750085418963c681717d312adaf9f0567f0d6310
Ally <allison.avis@gmail.com>
2018-12-03 21:30:29 +0000

    created gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,24 @@
 var request = require('request');
+var token = requre('/secrets').GITHUB_TOKEN;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-
-
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+    if (repoOwner === undefined || repoName === undefined) {
+      throw new Error("Please enter repoOwner and repoName");
+    }
+
+    var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + token
+    }
+  };
+
+  request(options, function(err, response, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
